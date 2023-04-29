9e9cf5b604ca9027e6663052ea25046bd6ce034d
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2023-04-29 12:01:53 -0300

    Pokemons axios request added

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,11 +1,17 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { startLoadingPokemons, setPokemons } from "./pokemonSlice";
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    // TODO: realizar petición http
+    // Realizar petición HTTP con fetch
+    // const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+    // const data = await response.json();
 
-    // dispatch(setPokemons());
+    // Realizar petición HTTP con axios
+    const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
